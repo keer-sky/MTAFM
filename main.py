9c92a34b78e20bc18fe64527f49283ac93486059
keer-sky <59358424+keer-sky@users.noreply.github.com>
2026-08-07 09:57:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,39 +2,36 @@ import torch
 from torch.utils.data import DataLoader, WeightedRandomSampler
 import os
 
-from dataset import EnhancedTimeSeriesDataset
+from dataset import Time_seriesDataset
 from model import Transformer_enc
 from trainer import Trainer
 from data_utils import prepare_datasets
 
 def main():
-    sequence_length = 8
+    Observe_length = 8
     prediction_length = 52
     batch_size = 32
     epochs = 200
     focus_classes = [1]
-    data_dir = 'data'
     model_dir = 'models'
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"device: {device}")
 
     data_dir = "data"
-    train_df, val_df = prepare_datasets(data_dir=data_dir)
+    train_data, val_data = prepare_datasets(data_dir=data_dir)
 
-    train_dataset = EnhancedTimeSeriesDataset(
-        train_df, sequence_length, prediction_length,
+    train_dataset = Time_seriesDataset(
+        train_data, Observe_length, prediction_length,
         augment_data=True, focus_classes=focus_classes
     )
     train_scaler = train_dataset.get_scaler()
     train_label_encoder = train_dataset.get_label_encoder()
-    val_dataset = EnhancedTimeSeriesDataset(
-        val_df, sequence_length, prediction_length,
+    val_dataset = Time_seriesDataset(
+        val_data, Observe_length, prediction_length,
         augment_data=False, focus_classes=focus_classes,
         scaler=train_scaler, label_encoder=train_label_encoder
     )
-    print(f"  Training set: {len(train_dataset)}")
-    print(f"  Validation set: {len(val_dataset)}")
+    print("train_dataset_num:",len(train_dataset))
+    print("val_dataset_num:",len(val_dataset))
 
     train_targets = train_dataset.encoded_labels
     class_weights = train_dataset.class_weights
@@ -64,8 +61,8 @@ def main():
         uncertainty_weighting=True, focus_classes=focus_classes
     )
 
-    history = trainer.train(train_loader, val_loader, epochs, lr=0.0001)
-    return model, train_dataset, history, device
+    metrics = trainer.train(train_loader, val_loader, epochs, lr=0.0001)
+    return metrics
 
 if __name__ == "__main__":
-    model, train_dataset, history, device = main()
+    metrics = main()
